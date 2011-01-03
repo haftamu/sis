c9fe1b11c251fee85dce68285f9ecc7a25b45bab
Yared Getachew <ygetachew@vedtechpro.com>
2011-01-03 10:51:01 +0300

    rearrangements made on acadmic calendar

--- app/models/academic_calendar.rb
+++ app/models/academic_calendar.rb
@@ -6,7 +6,17 @@ class AcademicCalendar < ActiveRecord::Base
 
 
     def name
-        [start_date.year,end_date.year].join('/')
+      [
+        [
+          '(',
+          [
+            start_date.year,end_date.year
+          ].join('/'),
+          ')'
+        ].join(' '),
+        start_date.to_formatted_s(:long),
+        end_date.to_formatted_s(:long)
+      ].join(' - ')
     end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,10 +2,19 @@ class Student < ActiveRecord::Base
     belongs_to :person
     belongs_to :department
     has_one :admission
-
-    def initialize
-      self.person = Person.new
+    after_initialize :init_person
+    after_save :save_person
+    
+    def init_person
+      if(self.person_id.nil?)
+        self.person = Person.new
+      end
     end
+
+    def save_person
+      self.person.save
+    end
+    
     def person_attributes=(person_attributes)
       person_attributes.each do |attributes|
         person.build(attributes)
